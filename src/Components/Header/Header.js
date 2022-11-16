5d94e9d38338afd90b8bca9e62ab67c6972af843
1farjana1 <farjanayeasmin696@gmail.com>
2022-11-16 18:58:40 +0600

    added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,14 +6,17 @@ import './Header.css'
 const Header = () => {
     return (
         <div>
-            <nav className="navbar bg-light">
-                <div className="container-fluid">
+            <nav className="navbar mt-2 mb-0">
+                <div>
                     <a className="navbar-brand header" href="/get-your-kick">
                         <FontAwesomeIcon className='fs-1 icon' icon={faBicycle}></FontAwesomeIcon>
                         <h3>Get Your Kick</h3>
                     </a>
                 </div>
             </nav>
+            <div>
+                <h2 className='header-part'>Select your Exercise</h2>
+            </div>
         </div>
 
     );
